625b0716de91d57e53d93fc67c62d7a77138bfa9
Luka Markovic <markovicl136@gmail.com>
2022-11-29 19:25:30 +0100

    prijava css

--- prijava.php
+++ prijava.php
@@ -1,27 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Prijava</title>
+    <link rel="stylesheet" type="text/css" href="css/prijava.css">
+
 </head>
+
 <body>
-<h1>Prijava</h1>
+
     <div id="frm">
-        <form method ="POST" action="termini.php">
+        <h1>Prijava</h1>
+        <br><br>
+        <form method="POST" action="termini.php">
             <div>
                 <label>Korisnicko ime</label><br>
                 <input type="text" name="korisnicko-ime" class="form-control" id="korisnicko-ime">
             </div>
-        <div class="form">
-            <label>Šifra</label>
-            <input type="password" name = "sifra" class="form-control" id="sifra">
-            <div id="passwordHelpBlock" class="form-text">
+            <br>
+            <div class="form">
+                <label>Šifra</label><br>
+                <input type="password" name="sifra" class="form-control" id="sifra">
+                <div id="passwordHelpBlock" class="form-text">
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary" id = "btn">Prijavi se</button>
+            <br>
+            <div id="sub">
+                <button type="submit" class="btn btn-primary" id="btn">Prijavi se</button>
+            </div>
         </form>
     </div>
 </body>
+
 </html>
